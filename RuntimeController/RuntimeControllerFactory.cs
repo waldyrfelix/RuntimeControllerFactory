20043acbd89ff364041b7d6e03133185b0ea6434
David Roth <david.roth@fusonic.net>
2011-04-02 18:08:44 +0200

    Latest changes

--- RuntimeController/RuntimeControllerFactory.cs
+++ RuntimeController/RuntimeControllerFactory.cs
@@ -1,3 +1,29 @@
+//
+// RuntimeControllerFactory.cs
+//
+// Author:
+// David Roth <david.roth@fusonic.net>
+//
+// Copyright (c) 2011 Fusonic GmbH (http://www.fusonic.net)
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,28 +35,23 @@ using System.IO;
 using System.Reflection;
 
 namespace Fusonic.Web.Mvc.RuntimeController
-{	
+{
 	public class RuntimeControllerFactory : DefaultControllerFactory
 	{
 		private readonly IRuntimeControllerPathProvider pathProvider;
 		private readonly List<Assembly> assemblies = new List<Assembly>();
-		
-		public RuntimeControllerFactory (IRuntimeControllerPathProvider pathProvider)
+
+		public RuntimeControllerFactory(IRuntimeControllerPathProvider pathProvider)
 		{
 			this.pathProvider = pathProvider;
 		}
-		
-		public void ReferenceAssembly (Assembly assembly)
+
+		public void ReferenceAssembly(Assembly assembly)
 		{
 			assemblies.Add(assembly);
 		}
-		
-		public override IController CreateController (RequestContext requestContext, string controllerName)
-		{
-			return base.CreateController (requestContext, controllerName);
-		}
-		
-		protected override IController GetControllerInstance (RequestContext requestContext, Type controllerType)
+
+		protected override IController GetControllerInstance(RequestContext requestContext, Type controllerType)
 		{
 			CompilerSettings settings = new CompilerSettings();
 			Report report = new Report(new ConsoleReportPrinter());
@@ -40,7 +61,7 @@ namespace Fusonic.Web.Mvc.RuntimeController
 			bool instanceCreated = false;
 			eval.ReferenceAssembly(typeof(Controller).Assembly);
 			
-			foreach(Assembly assembly in assemblies)
+			foreach (Assembly assembly in assemblies)
 			{
 				eval.ReferenceAssembly(assembly);
 			}
@@ -50,38 +71,27 @@ namespace Fusonic.Web.Mvc.RuntimeController
 			CSharpControllerFile controllerFile = CSharpControllerFile.Parse(File.ReadAllText(path));
 			
 			eval.Run(controllerFile.ClassSource);
-			eval.Evaluate("new " + controllerName  + "();", out instance, out instanceCreated);			
+			eval.Evaluate("new " + controllerName + "();", out instance, out instanceCreated);
 			
 			return (IController)instance ?? base.GetControllerInstance(requestContext, controllerType);
 		}
-		
-		protected string GetControllerName(RequestContext requestContext, Type controllerType)
+
+		protected virtual string GetControllerName(RequestContext requestContext, Type controllerType)
 		{
 			string controllerName;
-			if(controllerType != null)
+			if (controllerType != null)
 			{
 				controllerName = controllerType.Name;
-			}
-			else
+			} else
 			{
 				controllerName = requestContext.RouteData.Values["controller"] as string;
-				if(controllerName != null)
+				if (controllerName != null)
 				{
 					controllerName += "Controller";
 				}
 			}
 			
 			return controllerName;
-		}
-		
-		public override void ReleaseController (IController controller)
-		{
-			base.ReleaseController (controller);
-		}
-		
-		protected override Type GetControllerType (RequestContext requestContext, string controllerName)
-		{
-			return base.GetControllerType (requestContext, controllerName);
 		}
 	}
 }
